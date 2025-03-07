21c3e9098cf06aed47c8fd70524eb88ffe2932e8
gloc54 <andreinevskiispb@gmail.com>
2025-03-07 13:58:29 +0300

    vscod add settings/angular-schematics

---
+++ src/app/pages/auth/Authorization/Authorization.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authorization',
+  imports: [],
+  templateUrl: './Authorization.component.html',
+  styleUrl: './Authorization.component.scss',
+})
+export class AuthorizationComponent { }
---
+++ src/app/pages/auth/Registration/Registration.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  imports: [],
+  templateUrl: './Registration.component.html',
+  styleUrl: './Registration.component.scss',
+})
+export class RegistrationComponent { }
