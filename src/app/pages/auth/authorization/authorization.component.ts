d4fa50762f3e5e3ccbb2c858c8e27f2f4b40200d
gloc54 <andreinevskiispb@gmail.com>
2025-03-10 18:25:50 +0300

    Registration and authorization forms created

--- src/app/pages/auth/authorization/authorization.component.ts
+++ src/app/pages/auth/authorization/authorization.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IUser } from '../../../models/user';
+import { UserService } from '../../../services/user.service';
+
 
 @Component({
   selector: 'app-authorization',
-  imports: [],
+  imports: [NgClass, FormsModule, ButtonModule, InputTextModule],
   templateUrl: './authorization.component.html',
   styleUrl: './authorization.component.scss',
 })
-export class AuthorizationComponent { }
+export class AuthorizationComponent implements OnInit, OnDestroy { 
+  login: string;
+  password: string;
+  
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {}
+
+  onAuth(): void {
+    const user: IUser = {
+      login: this.login,
+      password: this.password
+    }
+  }
+}
--- src/app/pages/auth/registration/registration.component.ts
+++ src/app/pages/auth/registration/registration.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../services/user.service';
+
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [NgClass, FormsModule, ButtonModule, CheckboxModule, InputTextModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
 })
-export class RegistrationComponent { }
+export class RegistrationComponent implements OnInit{ 
+  login: string;
+  password: string;
+  passwordRepeat: string;
+  cardNumber: string;
+  email: string;
+  isRemember: boolean;
+  labelText = "Сохранить пользователя в хранилище";  
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void 
+  {}
+
+
+  onAuth(ev: Event): void {
+    this.userService.addUser({login: this.login, password: this.password})
+  }
+
+}
