5113cf56b8f8dee603fcd10e34a30bf02925ccc5
gloc54 <andreinevskiispb@gmail.com>
2025-03-18 22:03:03 +0300

    Template Tours added

--- src/app/shared/api/index.ts
+++ src/app/shared/api/index.ts
@@ -3,5 +3,6 @@ import { environment } from "../../../environments/environment.development";
 const serverIp = environment.apiUrl;
 export const API = {
     auth: `${serverIp}/auth`,
-    registration: `${serverIp}/register`
+    registration: `${serverIp}/register`,
+    tours: `${serverIp}/tours`
 }
